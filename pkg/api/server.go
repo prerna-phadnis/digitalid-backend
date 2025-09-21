28caccab9ccc8fbfe6760be61915252aa5e4f255
AnujSuratran21 <anujsuratran21@gmail.com>
2025-09-21 15:27:36 +0530

    Update server.go

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -7,18 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// Start initializes and runs the Gin server
+
 func Start() {
-	// db, err := database.InitDB()
-	// if err != nil {
-	// 	log.Fatal("Could not connect to the database: ", err)
-	//}
+	
+	handlers.InitBlockchain()
 
 	router := gin.Default()
 
-	// r.LoadHTMLGlob("templates/*")
-
+	
 	handlers.SetupRoutes(router)
+
 	fmt.Println("Server is running on port 8085")
 	router.Run(":8085")
 }
